db9a1c58936024d4521ceb7a5c5cd0b380bb616b
M.D <marcio.daniel@msn.com>
2024-02-24 00:31:50 -0300

    feat(redux): add redux-persist

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { store } from './store/index'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { store, persistor } from './store/index'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-  <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </React.StrictMode>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </PersistGate>
   </Provider>
 )
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './reducers/user-reducer'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user']
+}
+
+const rootReducer = combineReducers({
+  user: userReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  }
+  reducer: persistedReducer
 })
+
+export const persistor = persistStore(store)
