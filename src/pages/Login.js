ef382735c257dd629a34f8f8db0eda99ba79eebc
valmir <valmirchagas88@gmail.com>
2024-02-27 21:05:20 -0300

    feat: create email and password validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import emailValidator from '../utils/emailValidator'
+import passwordValidator from '../utils/passwordValidator'
 
 const Login = () => {
   const navigate = useNavigate()
+  const [idLoginValid, setIsLoginValid] = useState(true)
+  const [dataLogin, setDataLogin] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (data) => {
+    setIsLoginValid(true)
+    setDataLogin({
+      ...dataLogin,
+      [data.name]: data.value
+    })
+  }
+  const { email, password } = dataLogin
+
+  const handleLogin = () => {
+    const emailIsValid = emailValidator(email)
+    const passwordIsValid = passwordValidator(password)
+
+    if (!emailIsValid || !passwordIsValid) {
+      setIsLoginValid(false)
+    }
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-gray-800 to-gray-900">
@@ -27,6 +52,8 @@ const Login = () => {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 placeholder="E-mail"
+                onChange={ (e) => handleChange(e.target) }
+                value={ email }
               />
             </div>
             <div>
@@ -41,6 +68,8 @@ const Login = () => {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 placeholder="Senha"
+                onChange={ (e) => handleChange(e.target) }
+                value={ password }
               />
             </div>
           </div>
@@ -67,7 +96,8 @@ const Login = () => {
 
           <div>
             <button
-              type="submit"
+              type="button"
+              onClick={() => handleLogin()}
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
               Logar
